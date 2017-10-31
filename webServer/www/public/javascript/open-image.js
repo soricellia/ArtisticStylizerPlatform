4f294b34ea82e3068d3eaa79530575d067239f1f
mstippa <ebinizerlop@gmail.com>
2017-10-31 15:38:55 -0400

    fixed the display of uploaded content

--- webServer/www/public/javascript/open-image.js
+++ webServer/www/public/javascript/open-image.js
@@ -1,13 +1,35 @@
 $(document).ready(function(){
-  // $(".openImage").on("click", function() {
-  //    $('.imagepreview').attr('src', $(this).find('img').attr('src'));
-  //    $('#image-modal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
-  // });
-// })
-// $(function() {
-		$('.pop').on('click', function() {
-			console.log($(this).find('img').attr('src'));
-			$('#imagepreview').attr('src', $(this).find('img').attr('src'));
-			$('#imagemodal').modal('show');
-		});
+
+  // dislplays the clicked on image in a modal
+	$('.pop').on('click', function() {
+		console.log($(this).find('img').attr('src'));
+		$('#imagepreview').attr('src', $(this).find('img').attr('src'));
+		$('#exploremodal').modal('show');
+	});
+
+  // calls readURL function when user uploads a photo
+  $("#uploadPhoto").change(function () {
+      readURL(this, 'uploaded-image');
+  });
+
+  // calls readURL function when user uploads a video
+  $("#uploadVideo").change(function () {
+      readURL(this, 'uploaded-image');
+  });
+
+  // displays the uploaded image or video in a modal
+  function readURL(input, id) {
+      if (input.files && input.files[0]) {
+          var reader = new FileReader();
+
+          reader.onload = function (e) {
+              $('#' + id).attr('src', e.target.result);
+              $('#homeModal').modal('show');
+          }
+
+          reader.readAsDataURL(input.files[0]);
+      }
+  }
 })
+
+
